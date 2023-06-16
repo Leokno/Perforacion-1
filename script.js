14591a752b32c6ac2816c12e5d08db7fd1bb1860
LM <leokno200@gmail.com>
2023-06-15 21:02:47 -0500

    last commit

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function FactorDeFriccion() {
+  const Densidad = parseFloat(document.getElementById("dendidadre").value);
+  const velocidad = parseFloat(document.getElementById("velocidadre").value);
+  const Diametro = parseFloat(document.getElementById("diametro").value);
+  const viscosidad = parseFloat(document.getElementById("viscocidad").value);
+  const Re = (928 * (Densidad * velocidad * Diametro)) / viscosidad;
+
+  document.getElementById("resultado0").innerHTML =
+    "El numero de reynolds es: " + Re.toFixed(3);
+}
+
+function FactorDeFriccion2() {
+  const Densidad = parseFloat(document.getElementById("dendidadre").value);
+  const velocidad = parseFloat(document.getElementById("velocidadre").value);
+  const Diametro = parseFloat(document.getElementById("diametro").value);
+  const viscosidad = parseFloat(document.getElementById("viscocidad").value);
+  const Re = (928 * (Densidad * velocidad * Diametro)) / viscosidad;
+  const FactorDeFriccion = 0.0791 / Math.pow(Re, 0.25);
+
+  document.getElementById("resultado01").innerHTML =
+    "El factor de friccion es : " + FactorDeFriccion.toFixed(3);
+}
+
 function CalcularPerdidaDePresion() {
   const Velocidad = parseFloat(document.getElementById("Velocidad").value);
   const ID = parseFloat(document.getElementById("IDTuberia").value);
@@ -47,10 +70,41 @@ function perdidaDePresionBroca() {
     document.getElementById("AreaBoquillas").value
   );
 
-  const perdidaDePresionBroca = (156 * Densidad * (ROP * ROP)) / areaBoquillas;
+  const perdidaDePresionBroca =
+    (156 * Densidad * (ROP * ROP)) / (areaBoquillas * areaBoquillas);
 
   document.getElementById("resultado3").innerHTML =
     "la perdida de presion en la broca es: " +
     perdidaDePresionBroca.toFixed(3) +
     " psi.";
+}
+
+function perdidaDePresionanularFL() {
+  const viscocidad = parseFloat(document.getElementById("viscosidad1").value);
+  const velocidad = parseFloat(document.getElementById("velocidad1").value);
+  const D1 = parseFloat(document.getElementById("D1").value);
+  const D2 = parseFloat(document.getElementById("D2").value);
+
+  const perdidaDePresionanularFL =
+    ((viscocidad * velocidad) / 1500) *
+    (D1 * D1 + D2 * D2 - (D2 * D2 - D1 * D1) / Math.log(D1 / D2));
+
+  document.getElementById("resultado4").innerHTML =
+    "la perdida de presion en el intervalo es:" +
+    perdidaDePresionanularFL.toFixed(3) +
+    "psi";
+}
+
+function PerdidadDepresionFLT() {
+  const viscocidad = parseFloat(document.getElementById("viscocidad6").value);
+  const velocidad = parseFloat(document.getElementById("valocidad6").value);
+  const diametro = parseFloat(document.getElementById("diametro7").value);
+
+  const PerdidadDepresionFLT =
+    (viscocidad * velocidad) / (1500 * (diametro * diametro));
+
+  document.getElementById("resultado5").innerHTML =
+    "la perdida de presion en la tuberia es: " +
+    PerdidadDepresionFLT.toFixed(4) +
+    "psi";
 }
